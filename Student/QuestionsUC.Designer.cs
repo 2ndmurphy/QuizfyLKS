62ab621801c8bded4996c4d24d31f9393e361980
2ndmurphy <muhammad.jibrilmuqoddas@gmail.com>
2025-11-06 12:16:20 +0700

    add(student): List of tracking subjects

--- Student/QuestionsUC.Designer.cs
+++ Student/QuestionsUC.Designer.cs
@@ -28,27 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.FlowSubjectContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.GreetLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // FlowSubjectContainer
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(258, 215);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Questions Student";
+            this.FlowSubjectContainer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FlowSubjectContainer.AutoScroll = true;
+            this.FlowSubjectContainer.Location = new System.Drawing.Point(20, 70);
+            this.FlowSubjectContainer.Name = "FlowSubjectContainer";
+            this.FlowSubjectContainer.Size = new System.Drawing.Size(597, 365);
+            this.FlowSubjectContainer.TabIndex = 16;
+            // 
+            // GreetLabel
+            // 
+            this.GreetLabel.AutoSize = true;
+            this.GreetLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GreetLabel.Location = new System.Drawing.Point(15, 16);
+            this.GreetLabel.Name = "GreetLabel";
+            this.GreetLabel.Size = new System.Drawing.Size(100, 25);
+            this.GreetLabel.TabIndex = 13;
+            this.GreetLabel.Text = "Questions";
             // 
             // QuestionsUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FlowSubjectContainer);
+            this.Controls.Add(this.GreetLabel);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "QuestionsUC";
             this.Size = new System.Drawing.Size(632, 450);
+            this.Load += new System.EventHandler(this.QuestionsUC_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +70,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel FlowSubjectContainer;
+        private System.Windows.Forms.Label GreetLabel;
     }
 }
--- Student/QuestionsUC.cs
+++ Student/QuestionsUC.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Quizfy_LKS.Student.Components;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.PerformanceData;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,35 @@ namespace Quizfy_LKS.Student
         public QuestionsUC()
         {
             InitializeComponent();
+        }
+
+        private void LoadQuestions()
+        {
+            using (var _db = new DataClasses1DataContext())
+            {
+                var questions = (from s in _db.Subjects
+                                 join q in _db.Questions on s.ID equals q.SubjectID into qs
+                                 select new
+                                 {
+                                     s.Name,
+                                     s.Time,
+                                     QuestionCount = qs.Count()
+                                 }).ToList();
+
+                FlowSubjectContainer.Controls.Clear();
+
+                foreach (var q in questions)
+                {
+                    SubjectCardUC card = new SubjectCardUC();
+                    card.SetData(q.Name, q.Time, q.QuestionCount);
+                    FlowSubjectContainer.Controls.Add(card);
+                }
+            }
+        }
+
+        private void QuestionsUC_Load(object sender, EventArgs e)
+        {
+            LoadQuestions();
         }
     }
 }
